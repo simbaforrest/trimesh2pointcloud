c03765d102dcd264e18c596c0561796d82209585
Chen Feng <simbaforrest at gmail dot com>
2017-11-05 12:33:15 -0500

    add 3rdparty c++ files

---
+++ 3rdparty/README.md
@@ -0,0 +1,2 @@
+Code downloaded from: http://rodolphe-vaillant.fr/images//codes/poisson_disk_wrapper_update.zip
+Blog of the code: http://rodolphe-vaillant.fr/?e=37
---
+++ 3rdparty/poisson_disk_wrapper/utils_sampling.hpp
@@ -0,0 +1,37 @@
+#include <vector>
+
+// =============================================================================
+namespace Utils_sampling {
+// =============================================================================
+
+struct Vec3 {
+    Vec3() { x = y = z = 0; }
+    Vec3(float x_, float y_, float z_) : x(x_), y(y_), z(z_) { }
+    float x,y,z;
+};
+
+/// @param radius : minimal radius between every samples. If radius <= 0
+/// a new radius is approximated given the targeted number of samples
+/// 'nb_samples'
+/// @param nb_samples : ignored if radius > 0 otherwise will try to match
+/// and find  nb_samples by finding an appropriate radius.
+/// @param verts : list of vertices
+/// @param nors : list of normlas coresponding to each verts[].
+/// @param tris : triangle indices in verts[] array.
+/// @code
+///     tri(v0;v1;v3) = { verts[ tris[ith_tri*3 + 0] ],
+///                       verts[ tris[ith_tri*3 + 1] ],
+///                       verts[ tris[ith_tri*3 + 2] ]   }
+/// @endcode
+/// @param [out] samples_pos : resulting samples positions
+/// @param [out] samples_nors : resulting samples normals associated to samples_pos[]
+/// @warning undefined behavior if (radius <= 0 && nb_samples == 0) == true
+void poisson_disk(float radius,
+                  int nb_samples,
+                  const std::vector<Vec3>& verts,
+                  const std::vector<Vec3>& nors,
+                  const std::vector<int>& tris,
+                  std::vector<Vec3>& samples_pos,
+                  std::vector<Vec3>& samples_nor);
+
+}// END UTILS_SAMPLING NAMESPACE ===============================================

c9bb4c18cd8bf25715ef7c65cfcf9b2dee765d51
yuqli <greenstone1564@gmail.com>
2019-04-17 23:20:09 -0400

    add run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+mkdir /media/yuqiong/DATA/city/nyc/nyc_pcds_from_triangle_sample
+python main.py -i /media/yuqiong/DATA/city/nyc/nyc_triangle_objs_sample -o /media/yuqiong/DATA/city/nyc/nyc_pcds_from_triangle_sample

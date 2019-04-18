5d83884a7ca911e2f23b31b8cfefbde838b78a7d
Ruoyu Wang <ruoyuwang@nyu.edu>
2019-04-17 23:23:32 -0400

    add run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-mkdir /media/yuqiong/DATA/city/nyc/nyc_pcds_from_triangle_sample
-python main.py -i /media/yuqiong/DATA/city/nyc/nyc_triangle_objs_sample -o /media/yuqiong/DATA/city/nyc/nyc_pcds_from_triangle_sample
+# mkdir /media/yuqiong/DATA/city/nyc/nyc_pcds_from_triangle_sample
+# python main.py -i /media/yuqiong/DATA/city/nyc/nyc_triangle_objs_sample -o /media/yuqiong/DATA/city/nyc/nyc_pcds_from_triangle_sample
+mkdir /data/city/zurich/zurich_pcds_from_triangle
+python main.py -i /data/city/zurich/zurich_triangle_objs -o /data/city/zurich/zurich_pcds_from_triangle

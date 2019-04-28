b8b2bf2e3070231b068ffbd23da8118b562de629
yuqli <greenstone1564@gmail.com>
2019-04-28 19:36:13 -0400

    refactor

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@
 # python main.py -i /media/yuqiong/DATA/city/zurich/test -o /media/yuqiong/DATA/city/zurich/
 # mkdir /data/city/zurich/zurich_pcds_from_triangle
 # python main.py -i /data/city/zurich/zurich_triangle_objs -o /data/city/zurich/zurich_pcds_from_triangle
-python main.py -i /data/city/nyc/nyc_tri_objs -o /data/city/nyc/nyc_tri_pcds
+# python main.py -i /data/city/nyc/nyc_tri_objs -o /data/city/nyc/nyc_tri_pcds
+python main.py -o /media/yuqiong/DATA/trimesh2pointcloud/test -i /media/yuqiong/DATA/trimesh2pointcloud/test
